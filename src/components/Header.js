1636c208522d49bab2c23c9428ec28fd02a55170
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-15 17:39:46 +0530

    Added the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='shadow-md bg-blue-500 text-white p-5 text-center'>
+        <h1 className='font-extrabold '>Movie Search</h1>
+    </div>
+  )
+}
+
+export default Header
