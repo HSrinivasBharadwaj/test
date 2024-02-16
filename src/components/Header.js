56c35acb4b4aea18483ebd1d7417d47c48e43b60
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-16 11:39:52 +0530

    Added the Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import banner from '../banner.jpg'
+import banner from '../banner.png'
 
 const Header = () => {
   return (
     <div className='shadow-md bg-blue-500 text-white p-5 flex justify-center items-center'>
-      <img src={banner} alt="Movie Logo" className="h-auto w-28 mr-2" /> 
+      <img src={banner} alt="Movie Logo" className="h-auto w-20 mr-2" /> 
       <h1 className='font-extrabold'>Movie Search</h1>
     </div>
   )
