55adbb19e0c21167582a8bd55b5375c5f935d447
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-16 11:01:44 +0530

    Added the Banner image to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import banner from '../banner.jpg'
 
 const Header = () => {
   return (
-    <div className='shadow-md bg-blue-500 text-white p-5 text-center'>
-        <h1 className='font-extrabold '>Movie Search</h1>
+    <div className='shadow-md bg-blue-500 text-white p-5 flex justify-center items-center'>
+      <img src={banner} alt="Movie Logo" className="h-auto w-28 mr-2" /> 
+      <h1 className='font-extrabold'>Movie Search</h1>
     </div>
   )
 }
