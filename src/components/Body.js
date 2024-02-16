a09d03902e58d251cfa7af55fb4fa70a6a69ac8f
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-16 11:01:14 +0530

    Added the mobile styles to the date range pickers

--- src/components/Body.js
+++ src/components/Body.js
@@ -59,39 +59,53 @@ const Body = () => {
         value={searchMovie}
         onChange={handleMovieChange}
       />
-      <div className="flex-col mt-5 mx-0 md:flex-row lg:flex-row xl:flex-row flex justify-center">
-        <label htmlFor="startDate">Start Date:</label>
+      <div className="flex-col m-5  md:flex-row lg:flex-row xl:flex-row flex justify-center items-center">
+        <label
+          htmlFor="startDate"
+          className="mx-3 my-2 md:my-3 lg:my-3 xl:my-3"
+        >
+          Start Date:
+        </label>
         <input
           type="date"
           id="startDate"
-          className="border border-gray-500 p-1 rounded-md mx-2"
+          className="border border-gray-500 p-1 rounded-md mt-2 md:mt-0 lg:mt-0 xl:mt-0"
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
         />
-        <label htmlFor="endDate">End Date:</label>
+        <label htmlFor="endDate" className="mx-3 my-2 md:my-3 lg:my-3 xl:my-3">
+          End Date:
+        </label>
         <input
           type="date"
           id="endDate"
-          className="border border-gray-500 p-1 rounded-md mx-2"
+          className="border border-gray-500 p-1 rounded-md mt-2 md:mt-0 lg:mt-0 xl:mt-0"
           value={endDate}
           onChange={(e) => setEndDate(e.target.value)}
         />
         <button
           onClick={handleFilterSubmit}
-          className="bg-blue-500 text-white p-2 rounded-md ml-2"
+          className="bg-blue-500 text-white p-2 rounded-md ml-2 mt-2 md:mt-0 lg:mt-0 xl:mt-0"
         >
           Submit
         </button>
       </div>
       <div className=" md:mx-32 lg:mx-32 xl:mx-32 my-10">
-        <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row justify-between md:flex-wrap lg:flex-wrap xl:flex-wrap mb-4">
-          {filteredMovie.map((movie) => {
-            return (
-              <div key={movie.id} className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3">
-                <MovieList movie={movie} />
-              </div>
-            );
-          })}
+        <div className=" flex flex-col md:flex-row lg:flex-row xl:flex-row justify-between md:flex-wrap lg:flex-wrap xl:flex-wrap mb-4">
+          {filteredMovie.length === 0 ? (
+            <p>No movies found within the specified date range.</p>
+          ) : (
+            filteredMovie.map((movie) => {
+              return (
+                <div
+                  key={movie.id}
+                  className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3"
+                >
+                  <MovieList movie={movie} />
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     </div>
